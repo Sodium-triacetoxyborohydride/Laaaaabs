364a39554efd19e9f629c7bed4c2ef0565929f41
Sodium-triacetoxyborohydride <tgaeev44@gmail.com>
2024-12-17 14:59:20 +0300

    исправлена ошибка, из-за которой код не всегда давал верную подсказку

--- processes2_wandering.py
+++ processes2_wandering.py
@@ -2,24 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
-# Инициализация сетки
-def initialize_grid(size):
-    return np.zeros((size, size))
-
-
-# Генерация матрицы вероятностей переходов
+# Генерация матрицы вероятностей переходов с учетом ограничений
 def generate_transition_matrix(size):
-    matrix = np.random.rand(size, size, 4)  # 4 направления: вверх, вниз, влево, вправо
-    for i in range(size):
-        for j in range(size):
-            matrix[i, j] /= np.sum(matrix[i, j])  # Нормировка вероятностей
-            if random.random() > 0.7:  # с вероятностью 0.3 оставляем 0
-                zero_index = random.randint(0, 3)
-                matrix[i, j][zero_index] = 0
-                matrix[i, j] /= np.sum(matrix[i, j])  # повторная нормировка
-    return matrix
+    matrix = [[[0] * 4 for _ in range(size)] for _ in range(size)]
+    for x in range(size):
+        for y in range(size):
+            directions = []
+            if x > 0:  # Влево
+                directions.append(0)
+            if x < size - 1:  # Вправо
+                directions.append(1)
+            if y > 0:  # Вверх
+                directions.append(2)
+            if y < size - 1:  # Вниз
+                directions.append(3)
 
+            probability_sum = 100
+            random.shuffle(directions)
+            for d in directions[:-1]:
+                probability = random.randint(0, probability_sum)
+                matrix[x][y][d] = probability / 100.0
+                probability_sum -= probability
+
+            # Убедиться, что сумма вероятностей равна 1
+            matrix[x][y][directions[-1]] = probability_sum / 100.0
+
+    return matrix
 
 # Выбор случайной позиции, исключая указанную
 def select_random_position(size, exclude=None):
@@ -28,18 +36,17 @@ def select_random_position(size, exclude=None):
         if pos != exclude:
             return pos
 
-
 # Реализация случайного блуждания
 def perform_random_walk(size, transition_matrix, start_pos, end_pos):
     steps = 0
     current_pos = start_pos
     path = [current_pos]
 
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # направления: вверх, вниз, влево, вправо
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # направления: влево, вправо, вверх, вниз
 
     while current_pos != end_pos:
         i, j = current_pos
-        move = np.random.choice(4, p=transition_matrix[i, j])  # выбор направления на основе вероятностей
+        move = random.choices(range(4), weights=transition_matrix[i][j])[0]  # выбор направления на основе вероятностей
         ni, nj = i + directions[move][0], j + directions[move][1]
 
         # проверка выхода за границы
@@ -49,7 +56,6 @@ def perform_random_walk(size, transition_matrix, start_pos, end_pos):
             steps += 1
 
     return steps, path
-
 
 # Проведение эксперимента
 def conduct_experiment(size, num_animals):
@@ -69,18 +75,20 @@ def conduct_experiment(size, num_animals):
     plt.ylabel('Frequency')
     plt.show()
 
-    # Визуализация пути
-    if size <= 3:
-        plt.figure()
+    # Визуализация пути первого животного
+    if size <= 10:
+        plt.figure(figsize=(6, 6))
         for i in range(len(path) - 1):
             plt.plot([path[i][1], path[i + 1][1]], [path[i][0], path[i + 1][0]], 'bo-')
         plt.plot(end_pos[1], end_pos[0], 'ro', label='Sensor')
-        plt.plot(start_pos[1], start_pos[0], 'go', label='Start')
+        plt.plot(path[0][1], path[0][0], 'go', label='Start')
         plt.gca().invert_yaxis()
+        plt.xlim(-1, size)
+        plt.ylim(-1, size)
+        plt.grid(True)
         plt.legend()
         plt.title('Path of an Animal')
         plt.show()
 
-
 # Запуск эксперимента с заданными параметрами
-conduct_experiment(size=10, num_animals=100)
+conduct_experiment(size=25, num_animals=100)
